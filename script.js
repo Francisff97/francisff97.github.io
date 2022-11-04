4f0d036fab5d86f532c13acaf19afe6b9364cc41
Francisff97 <67428751+Francisff97@users.noreply.github.com>
2022-11-04 16:22:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let em;
 document.getElementById('conv').onclick = function(){
 
     inizio = parseInt(document.getElementById('nIniziale').value);
-    pixel = inizio/1.33;
-    em = pixel/16;
+    pixel = inizio/1.33.toFixed(4) ;
+    em = pixel/16.toFixed(4) ;
   document.getElementById('px').value = pixel;
   document.getElementById('em').value = em;
 }
